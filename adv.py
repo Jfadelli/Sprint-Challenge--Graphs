8853ae84b0c39587fa14fb833d66fba0184c41be
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-10-09 09:04:51 -0700

    mvp995

--- adv.py
+++ adv.py
@@ -1,13 +1,11 @@
 from room import Room
 from player import Player
 from world import World
-
-import random
+from hero import Hero
 from ast import literal_eval
 
 # Load world
 world = World()
-
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -22,14 +20,19 @@ world.load_graph(room_graph)
 
 # Print an ASCII map
 world.print_rooms()
-
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
+##Create hero to complete the maze!
+hero = Hero(world, world.starting_room)
+hero.make_path()
 
+traversal_path = hero.path
+
+# My traversal graph 
+# print(hero.traversal_graph)
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
@@ -45,18 +48,16 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
-
-
+   
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+# 	cmds = input("-> ").lower().split(" ")
+# 	if cmds[0] in ["n", "s", "e", "w"]:
+# 		player.travel(cmds[0], True)
+# 	elif cmds[0] == "q":
+# 		break
+# 	else:
+# 		print("I did not understand that command.")
---
+++ hero.py
@@ -0,0 +1,109 @@
+from room import Room
+from player import Player
+from world import World
+import random
+
+# Create a stack
+class Stack:
+	def __init__(self):
+		self.stack = []
+        
+	def size(self):
+		return len(self.stack)
+
+	def push(self, value):
+		self.stack.append(value)
+
+	def pop(self):
+		if self.size() > 0:
+			return self.stack.pop()
+		else:
+			return None
+
+class Hero:
+	def __init__(self, world, starting_room):
+		self.path = []
+		self.world = world
+		self.direction_stack = Stack()
+		self.visited = set()
+		self.player = Player(starting_room)
+		self.traversal_graph = {}
+
+	def update_traversal_graph(self):
+		# input: 	current room
+		# output: 	create new, initialized entries for traversal graph
+
+		if self.player.current_room.id not in self.traversal_graph:
+			self.traversal_graph[self.player.current_room.id] = {}
+
+		for direction in self.player.current_room.get_exits():
+			if direction not in self.traversal_graph[self.player.current_room.id]:
+				self.traversal_graph[
+					self.player.current_room.id][direction] = '?'
+
+
+	def reverse(self, direction):
+		# input: 	single cardinal direction // returns: opposite cardinal direction when called
+
+		opposite_direction = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+		return opposite_direction[direction]
+
+	def move(self, direction):
+		# input: 	single cardinal direction
+		# output: 	moves player character and updates traversal graph for both the 
+        # 			previous and new room the player has occupied 
+
+		self.update_traversal_graph()
+		from_room = self.player.current_room.id
+		self.player.travel(direction)
+		self.path.append(direction)
+		self.update_traversal_graph()
+		if self.traversal_graph[from_room][direction] == '?':
+
+			self.traversal_graph[from_room][direction] = self.player.current_room.id
+
+			self.traversal_graph[self.player.current_room.id][self.reverse(direction)] = from_room
+
+	def make_path(self):
+		# Start traversing the world.
+		# Loops until stack is empty or all rooms have been visited
+
+		for direction in self.player.current_room.get_exits():
+			self.direction_stack.push((direction, "?"))
+
+		# add current room to visited
+		self.visited.add(self.player.current_room)
+
+		#traverse maze until all rooms visited and/or stack is empty
+		while self.direction_stack.size() > 0:
+			if len(self.visited) == len(self.world.rooms):
+				return
+
+			direction_info = self.direction_stack.pop()
+
+			#check if room has been visited, or is marked as unknown
+			if direction_info[1] is "?":
+
+				if self.player.current_room.get_room_in_direction(direction_info[0]) not in self.visited:
+					self.move(direction_info[0])
+					self.visited.add(self.player.current_room)
+					self.add_directions(direction_info[0])
+
+			else:
+				self.move(direction_info[0])
+
+	def add_directions(self, last_direction):
+
+		# input: 	direction last traveled
+		# output: 	updates stack with reverse direction for backtracking and new 
+		# 			directions if unvisited rooms are found in neighboring rooms.
+
+		self.direction_stack.push((self.reverse(last_direction), "visited"))
+		available_directions = self.player.current_room.get_exits()
+		random.shuffle(available_directions)
+
+		for direction in available_directions:
+			room = self.player.current_room.get_room_in_direction(direction)
+
+			if room not in self.visited:
+				self.direction_stack.push((direction, "?"))
